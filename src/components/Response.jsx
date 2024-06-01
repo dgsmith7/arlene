a80620a7a837af90c6750ddd9a342a63f334f9df
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2024-05-31 21:43:57 -0500

    removed dist tracking

--- src/components/Response.jsx
+++ src/components/Response.jsx
@@ -4,41 +4,6 @@
 const Response = ({ advice }) => {
   // const [advice, setAdvice] = useState("");
 
-  // useEffect(() => {
-  //   console.log("Query - ", { query });
-  //   setAdvice("This is where the returned advice will be displayed.\n", {
-  //     query,
-  //   });
-
-  //   // let url = "http://localhost:3000";
-  //   // //    url = "https://bbc-backend-rg9z5.ondigitalocean.app";
-  //   // setDbUrl(url);
-  //   // let url = JSON.parse(localStorage.getItem("dbUrl"));
-
-  //   // const getAdvice = async () => {
-  //   //   let headersList = {
-  //   //     Accept: "*/*",
-  //   //     "Content-Type": "application/x-www-form-urlencoded",
-  //   //   };
-  //   //   let bodyContent = { query };
-
-  //   //   await fetch(`${url}/submit-query`, {
-  //   //     method: "POST",
-  //   //     credentials: "include", // to send HTTP only cookies
-  //   //     body: bodyContent,
-  //   //     headers: headersList,
-  //   //   })
-  //   //     .then((response) => response.text())
-  //   //     .then((r) => {
-  //   //       setAdvice(r.data);
-  //   //     })
-  //   //     .catch((error) => {
-  //   //       console.log("error - ", error);
-  //   //     });
-  //   // };
-  //   // getAdvice();
-  // }, []);
-
   return (
     <div className="p-4 bg-white text-bacon_black-700 dark:bg-bacon_black-700 dark:text-white">
       <div className="min-h-screen">
